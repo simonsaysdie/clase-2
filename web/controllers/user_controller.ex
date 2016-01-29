3615d74a2676a3d3e510b7c3e73b7265c4daa3ee
Alejandro Martínez <simonsays0704@gamil.com>
2016-01-29 11:12:51 -0600

    Add register form

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -1,8 +1,11 @@
 defmodule Clase2.UserController do
   use Clase2.Web, :controller
 
+  alias Dennis.User
+
   def register(conn, %{"user" = _}) do
-  	
+  	changeset = User.changeset(%User{})
+  	render(conn, "register.html", user: changeset)
   end
 
   def register(conn, _) do
